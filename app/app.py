744541fc0be2d9bba4a8c742f8d114de29d10270
Felipe Martins <eufmartins@hotmail.com>
2025-03-15 14:11:04 +0000

    Desafio Traefik

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Olá! Este é um app Flask rodando com Traefik!"
+    return "Flask App ON!!!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
